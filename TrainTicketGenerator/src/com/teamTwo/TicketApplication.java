c295324833c8149a431e3fbe67d0b32e598b919b
Swapna <potala.swapna@gmail.com>
2019-06-27 09:39:23 -0400

    TrainDAO class

--- TrainTicketGenerator/src/com/teamTwo/TicketApplication.java
+++ TrainTicketGenerator/src/com/teamTwo/TicketApplication.java
@@ -1,10 +1,66 @@
 package com.teamTwo;
 
-public class TicketApplication {
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
+import static java.lang.System.exit;
+
+public class TicketApplication {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scr = new Scanner(System.in);
+
+		try {
+
+			System.out.println("Enter travel Date");
+			String dateStr = scr.nextLine();
+			System.out.println("Entered Date is " + dateStr);
+
+			System.out.println("Enter the Train number");
+			int trainNo = scr.nextInt();
+			Train trn = TrainDAO.findTrain(trainNo);
+			if (trn == null) {
+				System.out.println("train with given train number does not exist");
+				exit(0);
+			}
+
+
+			String pattern = "dd/MM/yyyy";
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+			Date travelDate = simpleDateFormat.parse(dateStr);
+			//TODO
+			Ticket ticket = new Ticket(travelDate, trn);//creating ticket object here with train and date
+
+//        System.out.println("Travel date is before current date ");
+			System.out.println("Enter Number of Passengers");
+			int psngrNo = scr.nextInt();
+
+
+			for (int i = 0; i < psngrNo; i++) {
+				Scanner psngrScanner = new Scanner(System.in);
+				System.out.println("Enter Passenger Name");
+				String name = psngrScanner.nextLine();
+
+				System.out.println("Enter age");
+				int age = psngrScanner.nextInt();
+
+				System.out.println("Enter Gender");
+				char gender = psngrScanner.next().charAt(0);
+
+				ticket.addPassenger(name, age, gender);//adding passenger to the ticket object
+			}
+
+			ticket.writeTicket();
+			//TODO
+			System.out.println("Ticket Booked with PNR : " + ticket.getPnr());
+
+
+		} catch (ParseException | SQLException e) {
+			e.printStackTrace();
+		}
+
 
 	}
-
 }
--- TrainTicketGenerator/src/com/teamTwo/TrainDAO.java
+++ TrainTicketGenerator/src/com/teamTwo/TrainDAO.java
@@ -1,5 +1,61 @@
 package com.teamTwo;
 
+import java.sql.*;
+
+
+import static java.sql.DriverManager.*;
+
 public class TrainDAO {
+    private String Driver_NAME;
+    private String DB_URL;
+    private String USER_NAME;
+    private String PASSWORD ;
+
+    public TrainDAO() {
+        Driver_NAME = "oracle.jdbc.driver.OracleDriver";
+        this.DB_URL = "jdbc:oracle:thin:@localhost:1521:xe";
+        this.USER_NAME = "hr";
+        this.PASSWORD = "hr";
+    }
+
+    public static Train findTrain(int trainNo) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            String url1="jdbc:oracle:thin:@localhost:1521:xe";
+            con = getConnection(url1,"hr","hr");
+            String query1="select * from TRAINS where TRAIN_NO = ? ";
+            pstmt = con.prepareStatement(query1);
+            //In the following statement 1st argument represent the
+            // parameter number mentioned in the above statement with ?
+            pstmt.setInt(1, trainNo);
+            rs = pstmt.executeQuery();
+            if(rs.next()) {
+                System.out.println(rs.getInt(1)+"...."+rs.getString(2)+"....."+rs.getString(3));
+                String trainName = rs.getString(2);
+                String source = rs.getString(3);
+                String destination = rs.getString(4);
+                int ticketPrice = rs.getInt(5);
+
+                Train trainObj = new Train(trainNo, trainName, source, destination, ticketPrice);
+                return trainObj;
+            } else {
+                return null;
+            }
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }catch (SQLException sqe) {
+            sqe.printStackTrace();
+        } finally {
+            rs.close();
+            pstmt.close();
+            con.close();
+        }
+        return null;
+    }
 
 }
+
